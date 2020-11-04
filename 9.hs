a48a34ab80cefd4b5ec74d1afefbd87cbfb6ba73
peranovicc <cvijanperanovic@aol.com>
2020-11-04 15:25:09 +0100

    Промењена провера length == 0, коришћење функције null (проверава да ли је листа празна)

--- 9.hs
+++ 9.hs
@@ -1,2 +1,3 @@
 prost 1 = False
-prost n = (length [x | x <- [2..(n-1)], n `mod` x == 0]) == 0
+prost n = null [x | x <- [2..(n-1)], n `mod` x == 0]
+-- prost n = (length [x | x <- [2..(n-1)], n `mod` x == 0]) == 0
